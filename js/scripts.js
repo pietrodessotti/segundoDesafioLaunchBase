f028e1d59a4e54b0dad23f7f29c8f170310d5454
Pietro Dessotti <pietrohdessotti@gmail.com>
2021-05-20 18:27:56 -0300

    finalização do desafio 2 do LaunchBase

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,22 @@ for (let post of posts) {
 }
 
 function closeModal() {
-    for (let post of posts) {
-        document.querySelector("iframe").removeAttribute
-        document.querySelector('.modalOverlay').classList.remove('active');
+    document.querySelector("iframe").removeAttribute
+    document.querySelector('.modalOverlay').classList.remove('active');
+}
+
+function maximizeModal() {
+    const maximize = document.querySelector('.modal')
+    const minimize = document.querySelector('.maximize')
+
+    if (maximize.contains(minimize) == true) {
+        document.querySelector('.modal').addEventListener("click", () => {
+            document.querySelector('.modal').classList.remove('maximize')
+        })
+    } else if (maximize.contains(minimize) == false) {
+        document.querySelector('.modal').addEventListener("click", () => {
+            document.querySelector('.modal').classList.add('maximize')
+        })
     }
 }
+
